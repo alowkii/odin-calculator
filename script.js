a91b9f6fa3c83270cc1c7b5767164c33d1b89c1b
fellowpandit <paalok333@gmail.com>
2024-06-24 23:17:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function append(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    display.value = eval(display.value);
+}
